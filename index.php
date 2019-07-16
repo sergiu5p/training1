1c7b383f7d5e6a13d744f0fd7a351fecaa023c35
Sergiu-Anton Purcariu <sergiu.purcariu@5psolutions.eu>
2019-07-16 18:52:34 +0300

    First commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+    session_start();
+    require_once "common.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>store</title>
+    </head>
+    <body>
+
+    </body>
+</html>
