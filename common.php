7077bb205dc527a495b870b803709df43298285c
Sergiu-Anton Purcariu <sergiu.purcariu@5psolutions.eu>
2019-07-17 13:16:49 +0300

    Upload to git

--- common.php
+++ common.php
@@ -20,7 +20,7 @@
     // Create connection to database
     $conn = new mysqli($serverName, $userName, $password, $dbname) or die($conn->connect_error);
 
-    // Select all the products not in cart Array
+    //  Select all the products not in cart Array
     $statement = $conn->prepare("SELECT * FROM products WHERE id NOT IN (?)");
     $statement->bind_param("s", $ids);
     $statement->execute();
