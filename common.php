f94f4b0f6cb967033f4ef284e833cd307439eb76
Sergiu-Anton Purcariu <sergiu.purcariu@5psolutions.eu>
2019-07-17 13:32:32 +0300

    Add $cartArray in the session.

--- common.php
+++ common.php
@@ -8,9 +8,10 @@
         return $data;
     }
 
-    // create the cart array
+    // create the cart array and add it in Session
     $cartArray = [];
     $ids = join("','", $cartArray);
+    $_SESSION["cartIds"] = $ids;
 
     $serverName = constant("server");
     $userName = constant("dbUsername");
@@ -20,8 +21,9 @@
     // Create connection to database
     $conn = new mysqli($serverName, $userName, $password, $dbname) or die($conn->connect_error);
 
-    //  Select all the products not in cart Array
+    // Select all the products not in cart Array
     $statement = $conn->prepare("SELECT * FROM products WHERE id NOT IN (?)");
-    $statement->bind_param("s", $ids);
+    $statement->bind_param("s", $_SESSION["cartIds"]);
     $statement->execute();
     $result = $statement->get_result();
+
