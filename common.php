b2196f25d839e36860b5c3fdfc1c8e2ae460aa92
Sergiu-Anton Purcariu <sergiu.purcariu@5psolutions.eu>
2019-07-17 12:48:21 +0300

    Create products table in index.php and $cartArray.

--- common.php
+++ common.php
@@ -8,6 +8,10 @@
         return $data;
     }
 
+    // create the cart array
+    $cartArray = [];
+    $ids = join("','", $cartArray);
+
     $serverName = constant("server");
     $userName = constant("dbUsername");
     $password = constant("dbPassword");
@@ -15,3 +19,9 @@
 
     // Create connection to database
     $conn = new mysqli($serverName, $userName, $password, $dbname) or die($conn->connect_error);
+
+    // Select all the products not in cart Array
+    $statement = $conn->prepare("SELECT * FROM products WHERE id NOT IN (?)");
+    $statement->bind_param("s", $ids);
+    $statement->execute();
+    $result = $statement->get_result();
--- index.php
+++ index.php
@@ -10,6 +10,29 @@
         <title><?= trans("store") ?></title>
     </head>
     <body>
-
+        <table>
+            <?php while($row = $result->fetch_assoc()): ?>
+            <tr>
+                <td>
+                <img src="img/<?= $row['id'] ?>.jpg" alt="<?= $row['title'] ?>" width="250" height="250">
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <?= $row['title']?>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <?= $row['description'] ?>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <?= $row['price'], "$"?>
+                </td>
+            </tr>
+            <?php endwhile; ?>
+        </table>
     </body>
 </html>
