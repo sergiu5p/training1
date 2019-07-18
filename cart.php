7ff8602d3758ed0c5ee5f188e413da13fd98eae9
sergiu5p <sergiu.purcariu@5psolutions.eu>
2019-07-19 00:42:58 +0300

    Add the products that are in cart

--- cart.php
+++ cart.php
@@ -1,5 +1,29 @@
 <?php
     session_start();
+    require_once "common.php";
     if( !isset($_SESSION["cartIds"]) ) {
         header("location: index.php");
+    } else {
+        $query = "SELECT * FROM products WHERE id IN (".implode(",", $_SESSION["cartIds"]).")";
+        $result = $conn->query($query) or die($conn->error);
     }
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title><?= trans("Cart") ?></title>
+    </head>
+    <body>
+    <div>
+        <?php while ( $row = $result->fetch_assoc() ):  ?>
+            <div>
+                <img alt="<?= test_input($row['title'])?>" src="img/<?= test_input($row['id']) ?>.jpg" width="150" height="150">
+                <h4><?= test_input($row["title"]) ?></h4>
+                <p><?= test_input($row["description"]) ?></p>
+                <h4><?= test_input($row["price"]) ?></h4>
+            </div>
+        <?php endwhile; ?>
+    </div>
+    </body>
+</html>
