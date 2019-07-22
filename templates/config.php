df93220c384927386e9f3cda49c00710f962042c
Sergiu-Anton Purcariu <sergiu.purcariu@5psolutions.eu>
2019-07-22 11:47:47 +0300

    Add username and password

--- templates/config.php
+++ templates/config.php
@@ -1,11 +1,15 @@
 <?php
 
-// database connection credentials
-// define("SERVER", "");
-// define("DBUSERNAME", "");
-// define("DBPASSWORD", "");
-// define("DBNAME", "");
+    // database connection credentials
+    // define("SERVER", "");
+    // define("DB_USERNAME", "");
+    // define("DB_PASSWORD", "");
+    // define("DB_NAME", "");
 
-// admin credentials
-// define("ADMINUSERNAME", "");
-// define("ADMINPASSWORD", "");
+    // username and password
+    //define("USER_NAME", "");
+    //define("USER_PASSWORD", "");
+
+    // admin credentials
+    // define("ADMIN_USERNAME", "");
+    // define("ADMIN_PASSWORD", "");
