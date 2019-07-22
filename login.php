df688c8d150f4f6ffbfea81fc30eaa261ab5cd8e
Sergiu-Anton Purcariu <sergiu.purcariu@5psolutions.eu>
2019-07-22 11:14:44 +0300

    Create login form

---
+++ login.php
@@ -0,0 +1,19 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lane="en">
+    <head>
+        <meta charset="UTF-8">
+        <title><?= trans("login") ?></title>
+    </head>
+    <body>
+        <div>
+            <form action="">
+                <input type="text" name="userName" placeholder="Username">
+                <input type="password" name="password">
+            </form>
+        </div>
+    </body>
+</html>
