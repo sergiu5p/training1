2dc401725487a9a442db0f15bee0010efbc6956d
Sergiu-Anton Purcariu <sergiu.purcariu@5psolutions.eu>
2019-07-22 13:34:33 +0300

    Create variable logged_in in the session.

--- login.php
+++ login.php
@@ -1,6 +1,25 @@
 <?php
-    require_once "common.php";
+    session_start();
     require_once "config.php";
+    require_once "common.php";
+
+    if (isset($_POST["checkout"])) {
+        $name = test_input($_POST["name"]);
+        $email = test_input($_POST["email"]);
+        $comments = test_input($_POST["comments"]);
+    }
+
+    if (isset($_POST["login"])) {
+        if ($_POST["username"] == USER_NAME && $_POST["password"] == USER_PASSWORD) {
+            $_SESSION["logged_in"] = true;
+        } else {
+            echo "Wrong user name or password!";
+        }
+    }
+
+    if (isset($_SESSION["logged_in"]) && $_SESSION["logged_in"]) {
+        // send email to the admin
+    }
 ?>
 <!DOCTYPE html>
 <html lane="en">
@@ -10,9 +29,10 @@
     </head>
     <body>
         <div>
-            <form action="">
-                <input type="text" name="userName" placeholder="<?= trans("Username") ?>">
-                <input type="password" name="<?= trans("Password") ?>">
+            <form action="<?= test_input("login.php") ?>" method="POST">
+                <input type="text" name="username" placeholder="<?= trans("Username") ?>" required>
+                <input type="password" name="password" placeholder="<?= trans("Password") ?>" required>
+                <button name="login"><?= trans("Log in") ?></button>
             </form>
         </div>
     </body>
