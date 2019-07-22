7133c11f8f0af9a254988ed3bbafc35af54feb0f
Sergiu-Anton Purcariu <sergiu.purcariu@5psolutions.eu>
2019-07-22 16:21:05 +0300

    Trying to figure out how to the email with the necessary information.

--- login.php
+++ login.php
@@ -17,14 +17,23 @@
         $email = test_input($_POST["email"]);
         $comments = test_input($_POST["comments"]);
         $message_products = "";
+
         $query = "SELECT * FROM products WHERE id IN (".implode(",", $_SESSION["cartIds"]).")";
         $result = $conn->query($query) or die($conn->error);
+
         while ($row = $result->fetch_assoc()) {
             $message_products.="<h4>".$row['title']."</h4>";
             $message_products.="<h4>".$row["description"]."</h4>";
             $message_products.="<h4>".$row["price"]." $</h4>";
         }
         $_SESSION["message"] = $message_products."<h4>".$name."</h4>"."<h4>".$email."</h4>"."<h4>".$comments."</h4>";
+        $to = ADMIN_EMAIL;
+        $sub = "order";
+        $msg = $_SESSION["message"];
+        $headers = "MIME-Version: 1.0" . "\r\n";
+        $headers .= "From:".$name."<".$email.">"."\r\n";
+        $headers .= $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+        mail($to, $sub, $msg, $headers);
     }
 ?>
 <!DOCTYPE html>
