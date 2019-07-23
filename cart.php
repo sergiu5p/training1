33d28d9ac4a24f8ab34518451cbcb08791dbe865
Sergiu-Anton Purcariu <sergiu.purcariu@5psolutions.eu>
2019-07-23 11:40:57 +0300

    Send mail when ckeckout button is clicked

--- cart.php
+++ cart.php
@@ -13,6 +13,27 @@
         $query = "SELECT * FROM products WHERE id IN (".implode(",", $_SESSION["cartIds"]).")";
         $result = $conn->query($query) or die($conn->error);
     }
+
+    $name = test_input($_POST["name"]);
+    $email = test_input($_POST["email"]);
+    $comments = test_input($_POST["comments"]);
+    $message_products = "";
+
+    while ($row = $result->fetch_assoc()) {
+        $message_products.="<h4>".$row['title']."</h4>";
+        $message_products.="<h4>".$row["description"]."</h4>";
+        $message_products.="<h4>".$row["price"]." $</h4>";
+    }
+
+    $_SESSION["message"] = $message_products."<h4>".$name."</h4>"."<h4>".$email."</h4>"."<h4>".$comments."</h4>";
+    $to = ADMIN_EMAIL;
+    $sub = "order";
+    $msg = $_SESSION["message"];
+    $headers = "MIME-Version: 1.0" . "\r\n";
+    $headers .= "From:".$name."<".$email.">"."\r\n";
+    $headers .= $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+    echo "E-mail sent succesfully (i guess)";
+    mail($to, $sub, $msg, $headers);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -32,7 +53,7 @@
                 </div>
             <?php endwhile; ?>
         </div>
-        <form action="<?= test_input("login.php") ?>" method="POST">
+        <form action="<?= test_input("cart.php") ?>" method="POST">
             <input type="text" name="name" placeholder="<?= trans("Name") ?>" required>
             <br>
             <br>
@@ -42,7 +63,7 @@
             <input type="text" name="comments" placeholder="<?= trans("Comments") ?>">
             <br>
             <br>
-            <button name="checkout"><?= trans("Checkout") ?></button>
+            <input type="submit" name="checkout" placeholder="<?= trans("Checkout") ?>">
         </form>
         <a href="<?= trans('index.php') ?>"><?= trans("Go to index") ?></a>
     </body>
