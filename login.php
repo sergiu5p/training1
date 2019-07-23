be781ea0967de081336ad50f4e439d4e555777a8
Sergiu-Anton Purcariu <sergiu.purcariu@5psolutions.eu>
2019-07-23 12:48:21 +0300

    Send mail from cart.php, not login.php

--- login.php
+++ login.php
@@ -2,39 +2,6 @@
     session_start();
     require_once "config.php";
     require_once "common.php";
-
-    if (isset($_POST["login"])) {
-        if ($_POST["username"] == USER_NAME && $_POST["password"] == USER_PASSWORD) {
-            $_SESSION["logged_in"] = true;
-        } else {
-            echo "Wrong user name or password!";
-        }
-    }
-
-    if (isset($_SESSION["logged_in"]) && $_SESSION["logged_in"] && isset($_POST["checkout"])) {
-        // send email to the admin
-        $name = test_input($_POST["name"]);
-        $email = test_input($_POST["email"]);
-        $comments = test_input($_POST["comments"]);
-        $message_products = "";
-
-        $query = "SELECT * FROM products WHERE id IN (".implode(",", $_SESSION["cartIds"]).")";
-        $result = $conn->query($query) or die($conn->error);
-
-        while ($row = $result->fetch_assoc()) {
-            $message_products.="<h4>".$row['title']."</h4>";
-            $message_products.="<h4>".$row["description"]."</h4>";
-            $message_products.="<h4>".$row["price"]." $</h4>";
-        }
-        $_SESSION["message"] = $message_products."<h4>".$name."</h4>"."<h4>".$email."</h4>"."<h4>".$comments."</h4>";
-        $to = ADMIN_EMAIL;
-        $sub = "order";
-        $msg = $_SESSION["message"];
-        $headers = "MIME-Version: 1.0" . "\r\n";
-        $headers .= "From:".$name."<".$email.">"."\r\n";
-        $headers .= $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-        mail($to, $sub, $msg, $headers);
-    }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -47,7 +14,7 @@
             <form action="<?= test_input("login.php") ?>" method="POST">
                 <input type="text" name="username" placeholder="<?= trans("Username") ?>" required>
                 <input type="password" name="password" placeholder="<?= trans("Password") ?>" required>
-                <button name="login"><?= trans("Log in") ?></button>
+                <input type="submit" name="login" placeholder="<?= trans("Login") ?>">
             </form>
         </div>
     </body>
