e4581daf3957a215253a35eec4b6bc4706d9bd4d
Sergiu-Anton Purcariu <sergiu.purcariu@5psolutions.eu>
2019-07-24 14:50:43 +0300

    Try to print the error message if something's wrong with the image.

--- product.php
+++ product.php
@@ -28,8 +28,51 @@
         //$stmt = $conn->prepare($query);
         //$stmt->bind_param("ssi", $title, $description, $price);
         // $stmt->execute();
-        if (isset($_FILES["image"])) {
 
+        if (isset($_FILES["image"])) {
+            $id = test_input($_POST["id"]);
+            $target_dir = "uploads/";
+            $target_file = $target_dir . basename($_FILES["image"]["name"]);
+            $uploadOk = 1;
+            $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+            // Check if image file is a actual image or fake image
+            if(isset($_POST["image"])) {
+                $check = getimagesize($_FILES["image"]["tmp_name"]);
+                if($check !== false) {
+                    $uploadOk = 1;
+                } else {
+                    echo "File is not an image.";
+                    $uploadOk = 0;
+                }
+            }
+            // Check file size
+            if ($_FILES["image"]["size"] > 500000) {
+                echo "Sorry, your file is too large.";
+                $uploadOk = 0;
+            }
+            // Allow certain file formats
+            if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+                && $imageFileType != "gif" ) {
+                echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+                $uploadOk = 0;
+            }
+            // Check if $uploadOk is set to 0 by an error
+            if ($uploadOk == 0) {
+                header("location: product.php?edit=".$id);
+            // if everything is ok, try to upload file
+            } else {
+                $tmp_name = $_FILES["image"]["tmp_name"];
+                $new_name = "img/".$id.".".$imageFileType;
+                if (move_uploaded_file($tmp_name, $new_name)) {
+                    if (isset($_SESSION["message"])) {
+                        unset($_SESSION["message"]);
+                    }
+                    header("location: products.php");
+                } else {
+                    $_SESSION["message"] = "Sorry, there was an error uploading your image.";
+                    header("location: product.php?edit=".$id);
+                }
+            }
         }
     }
 ?>
@@ -41,6 +84,9 @@
     </head>
     <body>
         <a href="login.php?logout"><?= trans("Logout") ?></a>
+        <?php if (isset($_SESSION["message"])): ?>
+            <h1><?= $_SESSION["message"] ?><h1>
+        <?php endif; ?>
         <div>
             <?php if ($action == "edit"): ?>
                 <?php $row = $result->fetch_assoc() ?>
