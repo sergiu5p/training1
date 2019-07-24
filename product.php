904db3139ab27c4cf7a92168056c03e7d4657535
Sergiu-Anton Purcariu <sergiu.purcariu@5psolutions.eu>
2019-07-24 11:34:47 +0300

    Add "Logout" button

--- product.php
+++ product.php
@@ -2,7 +2,7 @@
     session_start();
     require_once "common.php";
 
-    if (!isset($_SESSION["logged_in"]) || $_SESSION["logged_in"] == false) {
+    if (!isset($_SESSION["logged_in"]) || !$_SESSION["logged_in"]) {
         header("location: login.php");
         exit();
     }
@@ -10,8 +10,11 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
+        <meta charset="UTF-8">
+        <title><?= trans("product") ?></title>
     </head>
     <body>
+        <a href="login.php?logout"><?= trans("Logout") ?></a>
 
     </body>
 </html>
