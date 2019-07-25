22b4d6910030665602b99b2efe079c2f1edcf2dc
Sergiu-Anton Purcariu <sergiu.purcariu@5psolutions.eu>
2019-07-25 16:41:22 +0300

    Create sqlInjection function.

--- common.php
+++ common.php
@@ -2,11 +2,18 @@
     session_start();
     require_once "config.php";
 
+     // Create connection to database
+    $conn = new mysqli(SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME) or die($conn->connect_error);
+
+    // For prevent sql injection
+    function sqlInjection($data) {
+        $data = strip_tags($data);
+        $data = mysqli_real_escape_string($GLOBALS['$conn'], $data);
+        return $data;
+    }
+
     // Define translate function
     function trans($data) {
         $data = htmlspecialchars($data);
         return $data;
     }
-
-     // Create connection to database
-    $conn = new mysqli(SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME) or die($conn->connect_error);
