09490b8561df001a12d87754cf0a01dd151e6a86
Sergiu-Anton Purcariu <sergiu.purcariu@5psolutions.eu>
2019-07-25 18:15:36 +0300

    Save the image when user click the button Save

--- product.php
+++ product.php
@@ -43,6 +43,47 @@
             $description = sqlInjection($_POST["description"]);
             $price = sqlInjection($_POST["price"]);
             $id = strip_tags($_POST["id"]);
+
+            if (isset($_FILES["image"])) {
+                $target_dir = "img/";
+                $target_file = $target_dir . basename($_FILES["image"]["name"]);
+                $uploadOk = 1;
+                $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+                // Check if image file is a actual image or fake image
+                if(isset($_POST["image"])) {
+                    $check = getimagesize($_FILES["image"]["tmp_name"]);
+                    if($check !== false) {
+                        $uploadOk = 1;
+                    } else {
+                        echo "File is not an image.";
+                        $uploadOk = 0;
+                    }
+                }
+                // Check file size
+                if ($_FILES["image"]["size"] > 500000) {
+                    echo "Sorry, your file is too large.";
+                    $uploadOk = 0;
+                }
+                // Allow certain file formats
+                if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+                    && $imageFileType != "gif" ) {
+                    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+                    $uploadOk = 0;
+                }
+                // Check if $uploadOk is set to 0 by an error
+                if ($uploadOk == 0) {
+                    header("location: product.php?id=".$id);
+                    // if everything is ok, try to upload file
+                } else {
+                    $tmp_name = $_FILES["image"]["tmp_name"];
+                    $new_name = "img/".$id.".".$imageFileType;
+
+                    @unlink($new_name);
+
+                    copy($tmp_name, $new_name);
+                }
+            }
+
             $stmt->execute();
         }
     }
