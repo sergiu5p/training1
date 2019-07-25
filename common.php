9fd72a075950fab2d200003a7c910ef7f634a865
Sergiu-Anton Purcariu <sergiu.purcariu@5psolutions.eu>
2019-07-25 14:51:09 +0300

    Delete test_input function and add htmlspecialchars in trans function

--- common.php
+++ common.php
@@ -4,14 +4,9 @@
 
     // Define translate function
     function trans($data) {
-        return $data;
-    }
-
-    function test_input($data) {
-        $data = trim($data);
         $data = htmlspecialchars($data);
         return $data;
-     }
+    }
 
      // Create connection to database
     $conn = new mysqli(SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME) or die($conn->connect_error);
