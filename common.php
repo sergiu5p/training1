2f9ff1915ffcd07b9d34b1ab29cd241844f8ceff
Sergiu-Anton Purcariu <sergiu.purcariu@5psolutions.eu>
2019-07-26 18:02:16 +0300

    Add imageValidation function. (test mode)

--- common.php
+++ common.php
@@ -17,3 +17,33 @@
         $data = htmlspecialchars($data);
         return $data;
     }
+
+    // unstable function. Check his functionality
+    function imageValidation($image)
+    {
+        $target_dir = "img/";
+        $target_file = $target_dir . basename($image["name"]);
+        $uploadOk = 1;
+        $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+        // Check if image file is a actual image or fake image
+        if (isset($_POST["image"])) {
+            $check = getimagesize($image["tmp_name"]);
+            if ($check) {
+                $uploadOk = 1;
+            } else {
+                $_SESSION['errors'][] =  "File is not an image.";
+                $uploadOk = 0;
+            }
+        }
+        // Check file size
+        if ($image["size"] > 500000) {
+            $_SESSION['errors'][] =  "Sorry, your file is too large.";
+            $uploadOk = 0;
+        }
+        // Allow certain file formats
+        if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg") {
+            $_SESSION['errors'][] =  "Sorry, only JPG, JPEG & PNG files are allowed.";
+            $uploadOk = 0;
+        }
+        return boolval($uploadOk);
+    }
