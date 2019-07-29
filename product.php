0316914ac3722e51ab4a9f12fd33a3cd273f37ab
Sergiu-Anton Purcariu <sergiu.purcariu@5psolutions.eu>
2019-07-29 11:38:41 +0300

    Make variable imageFileType global

--- product.php
+++ product.php
@@ -45,35 +45,12 @@
             $id = strip_tags($_POST["id"]);
 
             if (isset($_FILES["image"])) {
-                $target_dir = "img/";
-                $target_file = $target_dir . basename($_FILES["image"]["name"]);
-                $uploadOk = 1;
-                $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-                // Check if image file is a actual image or fake image
-                if (isset($_POST["image"])) {
-                    $check = getimagesize($_FILES["image"]["tmp_name"]);
-                    if ($check) {
-                        $uploadOk = 1;
-                    } else {
-                        $_SESSION['errors'][] =  "File is not an image.";
-                        $uploadOk = 0;
-                    }
-                }
-                // Check file size
-                if ($_FILES["image"]["size"] > 500000) {
-                    $_SESSION['errors'][] =  "Sorry, your file is too large.";
-                    $uploadOk = 0;
-                }
-                // Allow certain file formats
-                if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg") {
-                    $_SESSION['errors'][] =  "Sorry, only JPG, JPEG & PNG files are allowed.";
-                    $uploadOk = 0;
-                }
-                // Check if $uploadOk is set to 0 by an error
-                if ($uploadOk) {
+
+                // Check the validation of image
+                if (imageValidation($_FILES["image"])) {
 
                     $tmp_name = $_FILES["image"]["tmp_name"];
-                    $new_name = "img/".$id.".".$imageFileType;
+                    $new_name = "img/".$id.".".$GLOBALS['$imageFileType'];
 
                     @unlink($new_name);
 
