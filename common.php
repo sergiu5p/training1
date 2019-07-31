ee0ac1fc9bdc8f6ad9ad181a1f679d081e1b847d
Sergiu-Anton Purcariu <sergiu.purcariu@5psolutions.eu>
2019-07-31 12:11:39 +0300

    Remove the sqlInjection function.

--- common.php
+++ common.php
@@ -5,20 +5,14 @@
      // Create connection to database
     $conn = new mysqli(SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME) or die($conn->connect_error);
 
-    // To prevent sql injection
-    function sqlInjection($data) {
-        $data = strip_tags($data);
-        $data = mysqli_real_escape_string($GLOBALS['conn'], $data);
-        return $data;
-    }
 
     // Define translate function
-    function trans($data) {
+    function trans($data)
+    {
         $data = htmlspecialchars($data);
         return $data;
     }
 
-    // unstable function. Check his functionality
     function imageValidation($image)
     {
         $target_dir = "img/";
