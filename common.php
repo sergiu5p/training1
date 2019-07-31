9781ee8b5c3a5b3b1c3eefbc761edd8e6efc759f
Sergiu-Anton Purcariu <sergiu.purcariu@5psolutions.eu>
2019-07-31 13:20:20 +0300

    Remove the imageValidation function.

--- common.php
+++ common.php
@@ -9,34 +9,5 @@
     // Define translate function
     function trans($data)
     {
-        $data = htmlspecialchars($data);
         return $data;
-    }
-
-    function imageValidation($image)
-    {
-        $target_dir = "img/";
-        $target_file = $target_dir . basename($image["name"]);
-        $uploadOk = 1;
-        $GLOBALS['imageFileType'] = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-        // Check if image file is a actual image or fake image
-
-        $check = getimagesize($image["tmp_name"]);
-        if ($check) {
-            $uploadOk = 1;
-        } else {
-            $_SESSION['errors'][] =  "File is not an image.";
-            $uploadOk = 0;
-        }
-        // Check file size
-        if ($image["size"] > 500000) {
-            $_SESSION['errors'][] =  "Sorry, your file is too large.";
-            $uploadOk = 0;
-        }
-        // Allow certain file formats
-        if ($GLOBALS['imageFileType'] != "jpg" && $GLOBALS['imageFileType'] != "png" && $GLOBALS['imageFileType'] != "jpeg") {
-            $_SESSION['errors'][] =  "Sorry, only JPG, JPEG & PNG files are allowed.";
-            $uploadOk = 0;
-        }
-        return boolval($uploadOk);
     }
