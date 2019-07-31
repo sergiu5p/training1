108d5818acd64801a572f109ed9365be1c17dacf
Sergiu-Anton Purcariu <sergiu.purcariu@5psolutions.eu>
2019-07-31 17:04:44 +0300

    Remove htmlspecialchars for $row["id"]

--- products.php
+++ products.php
@@ -49,12 +49,12 @@
         </ul>
             <?php foreach ($rows as $row): ?>
                 <div>
-                    <img alt="<?= htmlspecialchars($row['title'])?>" src="img/<?= htmlspecialchars($row['id']) ?>" width="150" height="150">
+                    <img alt="<?= htmlspecialchars($row['title'])?>" src="img/<?= $row["id"] ?>" width="150" height="150">
                     <h4><?= htmlspecialchars($row["title"]) ?></h4>
                     <p><?= htmlspecialchars($row["description"]) ?></p>
                     <h4><?= htmlspecialchars($row["price"]) ?> $</h4>
-                    <a href="product.php?id=<?= $row['id'] ?>"><?= trans("Edit") ?></a>
-                    <a href="products.php?id=<?= $row['id'] ?>"><?= trans("Delete") ?></a>
+                    <a href="product.php?id=<?= $row["id"] ?>"><?= trans("Edit") ?></a>
+                    <a href="products.php?id=<?= $row["id"] ?>"><?= trans("Delete") ?></a>
                 </div>
             <?php endforeach; ?>
         <br>
