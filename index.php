24f0ae999b2fbccc73d3e72b82d3f674ff7cb8bb
Sergiu-Anton Purcariu <sergiu.purcariu@5psolutions.eu>
2019-08-06 17:31:49 +0300

    order the products by title

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     // select products that are not in the cart
     if (isset($_SESSION["cartIds"]) && $_SESSION["cartIds"]) {
         $in = join(',', array_fill(0, count($_SESSION["cartIds"]), "?"));
-        $query = "SELECT * FROM products WHERE id NOT IN ($in) ORDER BY id";
+        $query = "SELECT * FROM products WHERE id NOT IN ($in) ORDER BY title";
         $stmt = $conn->prepare($query);
         $stmt->bind_param(str_repeat("i", count($_SESSION["cartIds"])), ...$_SESSION["cartIds"]);
         $stmt->execute() or die($conn->error);
