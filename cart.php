49eb3f186f3bb6745fc64a93140cdea926e4a840
Sergiu-Anton Purcariu <sergiu.purcariu@5psolutions.eu>
2019-08-06 17:32:17 +0300

    order the products by title

--- cart.php
+++ cart.php
@@ -11,7 +11,7 @@
         exit();
     } else {
         $in = join(",", array_fill(0, count($_SESSION["cartIds"]), "?"));
-        $query = "SELECT * FROM products WHERE id IN ($in) ORDER BY id";
+        $query = "SELECT * FROM products WHERE id IN ($in) ORDER BY title";
         $stmt = $conn->prepare($query);
         $stmt->bind_param(str_repeat("i", count($_SESSION["cartIds"])), ...$_SESSION["cartIds"]);
         $stmt->execute() or die($conn->error);
