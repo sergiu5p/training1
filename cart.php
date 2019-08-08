ed55e5e5d5197bec5ab265e477aa6cd94f6ed85b
Sergiu-Anton Purcariu <sergiu.purcariu@5psolutions.eu>
2019-08-08 14:16:53 +0300

    ORDER BY id instead ORDER BY title

--- cart.php
+++ cart.php
@@ -14,7 +14,7 @@
         exit();
     }
     $in = join(",", array_fill(0, count($_SESSION["cartIds"]), "?"));
-    $query = "SELECT * FROM products WHERE id IN ($in) ORDER BY title";
+    $query = "SELECT * FROM products WHERE id IN ($in) ORDER BY id";
     $stmt = $conn->prepare($query);
     $stmt->bind_param(str_repeat("i", count($_SESSION["cartIds"])), ...$_SESSION["cartIds"]);
     $stmt->execute() or die($conn->error);
