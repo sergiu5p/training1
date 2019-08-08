51f517608ee7bb27c8d854e805d33c27cbdd1b8d
Sergiu-Anton Purcariu <sergiu.purcariu@5psolutions.eu>
2019-08-08 12:31:06 +0300

    Load image with extension

--- cart.php
+++ cart.php
@@ -92,7 +92,7 @@
         <div>
             <?php foreach ($rows as $row): ?>
                 <div>
-                    <img alt="<?= htmlspecialchars($row["title"]) ?>" src="img/<?= $row["id"] ?>" width="150" height="150">
+                    <img alt="<?= htmlspecialchars($row["title"]) ?>" src="img/<?= $row["id"].".".$row["image_extension"] ?>" width="150" height="150">
                     <h4><?= htmlspecialchars($row["title"]) ?></h4>
                     <p><?= htmlspecialchars($row["description"]) ?></p>
                     <h4><?= htmlspecialchars($row["price"]) ?> $</h4>
