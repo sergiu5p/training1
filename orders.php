46d440313696a320ec28f865addc4a95bfbb9465
Sergiu-Anton Purcariu <sergiu.purcariu@5psolutions.eu>
2019-08-09 10:44:42 +0300

    Add blank lines for readability

--- orders.php
+++ orders.php
@@ -2,17 +2,20 @@
     require_once "common.php";
 
     if (!isset($_SESSION["logged_in"]) || !$_SESSION["logged_in"]) {
+
         header("location: login.php");
         exit();
     }
 
     $rows = [];
 
+    // MAX 120 characters in one line
     $result = $conn->query("SELECT orders.*, SUM(price) AS summed_price FROM order_product JOIN orders ON 
         order_product.order_id=orders.id JOIN products ON order_product.product_id=products.id GROUP BY 
         order_product.order_id") or die($conn->error);
 
     if (mysqli_num_rows($result)) {
+
         while ($row = $result->fetch_assoc()) {
             $rows[] = $row;
         }
